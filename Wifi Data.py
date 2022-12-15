2eaa51a12fea3acf8737304bfac6ccadfae89e48
adetomiwanjoku@tfl.gov.uk <adetomiwanjoku@tfl.gov.uk>
2022-12-15 09:30:18 +0000

    Changed headers

--- Wifi Data.py
+++ Wifi Data.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+# MAGIC %md #Creating The Environment 
+
+# COMMAND ----------
+
 import numpy as np
 import pandas as pd
 import pyspark.pandas as ps
@@ -18,16 +22,16 @@ import networkx
 
 # COMMAND ----------
 
-df_02 = ps.read_parquet("dbfs:/FileStore/wifi/wifi-cleansed/2022/10/01/00/cleansedwifitraps_20221002040917.parquet")
+# MAGIC %md # Reading In Files 
+
+# COMMAND ----------
+
+df_02 = pd.read_parquet("dbfs:/FileStore/wifi/wifi-cleansed/2022/10/01/00/cleansedwifitraps_20221002040917.parquet")
 
 
 # COMMAND ----------
 
-df_03 = ps.read_parquet('dbfs:/FileStore/wifi/wifi-cleansed/2022/10/02/00/cleansedwifitraps_20221003040847.parquet')
-
-# COMMAND ----------
-
-
+df_03 = pd.read_parquet('/dbfs/FileStore/wifi/wifi-cleansed/2022/10/02/00/cleansedwifitraps_20221003040847.parquet')
 
 # COMMAND ----------
 
@@ -51,7 +55,24 @@ Elizabeth_line = df_03.loc[(df_03.AccessPointPrefix == 'heath4', 'heath1')]
 
 # COMMAND ----------
 
+meta = pd.read_csv('/dbfs/FileStore/wifi/station mapping/2020/06/23/07/nonlulinks_20200623072758.csv')
 
+meta.display()
+
+# COMMAND ----------
+
+path_sequence = pd.read_csv('/dbfs/FileStore/wifi/station mapping/2019/12/09/16/nodes_20191209161231.csv/part_00000_tid_5965681666348671588_1eee92ac_1587_4cb0_a1c5_7bf0d8692d6a_50809_1_c000.csv')
+
+# COMMAND ----------
+
+path_sequence.display()
+
+# Is ticket hall lobby in this dataset 
+
+# COMMAND ----------
+
+wap_location = path_sequence[['ZoneDescription','AtAp']].copy()
+wap_location.display()
 
 # COMMAND ----------
 
@@ -62,15 +83,28 @@ df_03.sort_values("CaptureTime").display()
 
 # COMMAND ----------
 
-df[["ClientMacAddress"]].count()
+oxfcir = df_03.loc[(df_03.AccessPointPrefix == "oxfcir")]
+oxfcir.display()
 
 # COMMAND ----------
 
-df[['ClientMacAddress']].max()
+oxfcir_wap_location =  pd.merge(oxfcir,wap_location, left_on='AccessPointName', right_on='AtAp', how='left')
 
 # COMMAND ----------
 
-g_address = df.loc[(df.ClientMacAddress == "Gary3qZD5s9ZcrWqPz7mFwN2Q+ySNB+LiaUa6u0tBLU=")]
+oxfcir_wap_location.display()
+
+# COMMAND ----------
+
+df_03[["ClientMacAddress"]].count()
+
+# COMMAND ----------
+
+df_03[['ClientMacAddress']].max()
+
+# COMMAND ----------
+
+g_address = df_03.loc[(df_03.ClientMacAddress == "Gary3qZD5s9ZcrWqPz7mFwN2Q+ySNB+LiaUa6u0tBLU=")]
 
 g_address.display()
 
@@ -90,13 +124,10 @@ g_address.sort_values(['CaptureTimeLocal']).display()
 x = df_03['ClientMacAddress'].unique().sum
 print(x)
 
-# COMMAND ----------
-
-
 
 # COMMAND ----------
 
-bond_street = df.loc[(df.AccessPointPrefix == "bondst") & (df.TravelDay == '2022-10-01')]
+bond_street = df_03.loc[(df_03.AccessPointPrefix == "bondst") & (df_03.TravelDay == '2022-10-01')]
 bond_street.display()
 
 # COMMAND ----------
@@ -150,15 +181,3 @@ df2['ClientMacAddress'].max()
 mac_address = df2.loc[(df2.ClientMacAddress == "zzneBJuF2Sq44WPKsQESjuvo1U0RADwIb04ppfek3OY=")]
 #g_address = df.loc[(df.ClientMacAddress == "Gary3qZD5s9ZcrWqPz7mFwN2Q+ySNB+LiaUa6u0tBLU=")]
 
-
-# COMMAND ----------
-
- df3 = mac_address.groupby('SeenPath').max()
-
-# COMMAND ----------
-
-df3
-
-# COMMAND ----------
-
-df2.info()
